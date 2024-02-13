de0a76cd7f112d5ad18a350769e527502167d728
AkselSukub <63051222+AkselSukub@users.noreply.github.com>
2024-02-13 17:13:18 +0300

    Upprog

--- prog/zadanie1.py
+++ prog/zadanie1.py
@@ -11,3 +11,8 @@ def calculate_geometric_mean(*args):
     product = prod(args)
     geometric_mean = product ** (1 / len(args))
     return geometric_mean
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 8, 10]
+    result = calculate_geometric_mean(*numbers)
+    print("Geometric mean:", result)
