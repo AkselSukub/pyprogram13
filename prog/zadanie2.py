706a6b78e9443a6e2392e7a21e36a0f6d820c420
AkselSukub <63051222+AkselSukub@users.noreply.github.com>
2024-02-13 17:16:16 +0300

    Upprog

--- prog/zadanie2.py
+++ prog/zadanie2.py
@@ -8,3 +8,8 @@ def calculate_harmonic_mean(*args):
     reciprocal_sum = sum(1 / num for num in args)
     harmonic_mean = len(args) / reciprocal_sum
     return harmonic_mean
+
+if __name__ == "__main__":
+    values = [2, 4, 6, 8]
+    result = calculate_harmonic_mean(*values)
+    print(f"The harmonic mean of {values} is: {result}")
